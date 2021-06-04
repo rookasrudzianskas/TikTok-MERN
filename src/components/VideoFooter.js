fc3ed3d4dc87fcaa1c4635b62a98f569b23b7373
Rokas <rookas.rudzenskas@gmail.com>
2021-06-04 13:22:12 +0300

    Rokas

--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./styles/VideoFooter.css";
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import Ticker from "react-ticker";
 
 const VideoFooter = () => {
     return (
@@ -7,7 +9,20 @@ const VideoFooter = () => {
             <div className="videoFooter__text">
                 <h3>@rookas</h3>
                 <p>This is some description</p>
+                <div className="videoFooter__ticker">
+                    <MusicNoteIcon className="videoFooter__icon" />
+
+                    <Ticker mode="smooth">
+                        {({ index }) => (
+                            <>
+                                <p>This is happy song</p>
+                            </>
+                        )}
+                    </Ticker>
+                </div>
             </div>
+
+            <img className="videoFooter__record" src="https://static.thenounproject.com/png/934821-200.png" alt=""/>
         </div>
     );
 };
