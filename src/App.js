98679d9288c3499f56c4c87211f10ff3957f3199
Rokas <rookas.rudzenskas@gmail.com>
2021-06-04 11:47:45 +0300

    Roaks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Video from "./components/Video";
 
 function App() {
   return (
     <div className="app">
-
+      <h1>Lets build TikTok 🚀</h1>
+        <Video />
     </div>
   );
 }
---
+++ src/components/Video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/Video.css";
+
+const Video = () => {
+    return (
+        <div className="video">
+            
+        </div>
+    );
+};
+
+export default Video;
