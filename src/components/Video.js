19e4fbc724c66ff26334c93a80229957c23cbcca
Rokas <rookas.rudzenskas@gmail.com>
2021-06-04 12:24:11 +0300

    Rokas

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import "./styles/Video.css";
 
 const Video = () => {
+
+    const videoRef = useRef(null);
+    const [playing, setPlaying] = useState(false);
+
+    const handleVideoPress = () => {
+        // if the video is playing, stop it, otherwise continue playing it
+        if(playing) {
+            videoRef.current.pause();
+            setPlaying(false)
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
+
+    }
+
     return (
         <div className="video">
-            <iframe className="video__player" width="500" height="800" src="https://www.youtube-nocookie.com/embed/UfGMj10wOzg?controls=0"
+            <iframe onClick={handleVideoPress} ref={videoRef} className="video__player" width="500" height="800" src="https://www.youtube-nocookie.com/embed/UfGMj10wOzg?controls=0"
                     title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen>
